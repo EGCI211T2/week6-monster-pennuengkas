4f79abf0edf8c73784ea2032ffc25041f75a10aa
pennuengkas <pennueng.kas@student.mahidol.edu>
2026-02-11 04:31:49 +0000

    in-class

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    monster A;
+  monster A("Siri", 100, 3);
+  monster B("Daisuke");
+
+  monster x[5]={{"James"},{"John",150}, {"Robert",200,5} , {"Michael",120,2}, {"William"} };
+  monster c;
+  monster*p;
+  p=&c;
+  p->display();
+  p=new monster("Sarina", 180, 5);
+  p->display();
+  delete p;
+
+  p=new monster[3];
+  delete []p;
+  cout<<"------------------"<<endl;
   /*
  
  monster *p,x;
--- monster.h
+++ monster.h
@@ -7,7 +7,28 @@ private:
 	int hp,potion;
 public:
 	void Attack(monster &);
-          void heal();
+    void heal();
+	void display();
 	monster(string, int,int);
+	~monster();
 };
+
+monster::monster(string n="anonymous", int h=1, int p=1){
+	name=n;
+	hp=h;
+	potion=p;
+	cout<<"A wild "<<name<<" appears!"<<endl;
+}
+
+void monster::display()
+{
+	cout<<"Name: "<<name<<endl;
+	cout<<"HP: "<<hp<<endl;
+	cout<<"Potion: "<<potion<<endl;
+}
+
+monster::~monster(){
+	cout<< name <<" is deleted."<<endl;
+}
+
 #endif
